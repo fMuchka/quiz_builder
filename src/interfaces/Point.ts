f5ac676a3aa22d0bc1b31155a8ca3a903c002819
fMuchka <37208551+fMuchka@users.noreply.github.com>
2021-12-19 18:37:53 +0100

    interfaces
    specification of used interfaces

---
+++ src/interfaces/Point.ts
@@ -0,0 +1,4 @@
+export default interface Points{
+    max: number;
+    incerement: number;
+}
---
+++ src/interfaces/QuizMessenger.ts
@@ -0,0 +1,10 @@
+import Theme from "@/classes/Theme";
+import Question from "@/classes/Question";
+import Media from "@/interfaces/Media";
+
+export default interface QuizMessenger{
+    flow: Theme["id"][];   // Theme IDs in order
+    themes: Record<Theme["id"], Theme>;   // Theme objects map
+    questions: Record<Question["id"], Question>;   // Question objects map
+    mediaFiles: Record<Media["id"], Media["path"]>;  // Media files
+}
