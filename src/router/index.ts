afac524156249ff1257f7d2038815e7b81f0f6ad
fMuchka <37208551+fMuchka@users.noreply.github.com>
2021-12-19 18:39:24 +0100

    update cli template

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,27 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    redirect: "/home"
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/home",
+    name: "Home",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/Home.vue"),
+  },
+  {
+    path: "/quizSetup",
+    name: "QuizSetup",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/QuizSetup.vue"),
   },
 ];
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,49 @@
-import { createStore } from "vuex";
+import { InjectionKey } from 'vue'
+import { createStore, Store } from "vuex";
+import { State } from '@/interfaces/State';
+import Quiz from '@/classes/Quiz';
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export default createStore<State>({
+  strict: process.env.NODE_ENV !== 'production',
+
+  state: {
+    questions: [], // storage of all questions created
+    themes: [],
+    mediaFiles: [],
+    quiz: new Quiz(),
+
+    tID: 0,
+    qID: 0,
+    mID: 0
+
+  },
+  mutations: {
+    setQuiz(state, q) {
+      state.quiz = q;
+    },
+
+    addTheme(state, t) {
+      state.themes.push(t);
+
+      state.quiz.addTheme(t);
+    },
+
+    incrementThemeID(state) {
+      state.tID++;
+    }
+  },
+  actions: {
+    createQuiz( { commit } ) {
+      commit("setQuiz", new Quiz);
+    }
+  },
   modules: {},
+  getters: {
+    quiz: ( state ) => { return state.quiz },
+
+    themeID: ( state ) => { return state.tID }
+  }
 });
