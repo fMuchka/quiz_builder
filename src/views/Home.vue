b921f38ed5dcdd33331dc9da81ffa45a05d1a506
fMuchka <37208551+fMuchka@users.noreply.github.com>
2021-12-19 18:40:52 +0100

    create quiz + add themes
    prove of concept: using Vue component manipulate TS defined structures
    - create quiz
    - add themes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+      <button @click="createQuiz()">Create Quiz</button>
+
+      <button disabled>Edit Quiz</button>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<script>
+export default {
+    name: "Home",
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    methods: {
+        createQuiz(){
+            this.$store.dispatch("createQuiz");
+
+            this.goNext();
+        },
+
+        goNext(){
+            this.$router.push("quizSetup")
+        }
+    }
+}
 </script>
+
+<style>
+
+</style>
---
+++ src/views/QuizSetup.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+
+    <div 
+        v-for="(item, key) in themes" 
+        :key="key"
+    >
+        {{ key }}
+    </div>
+
+    <button @click="addTheme()">T++</button>
+
+  </div>
+</template>
+
+<script>
+import Theme from '@/classes/Theme'
+export default {
+    name: "QuizSetup",
+
+    computed: {
+        quiz(){
+            return this.$store.getters["quiz"]
+        },
+
+        themes(){
+            return this.quiz.quizThemes
+        }
+    },
+
+    methods: {
+        addTheme(){
+            const id = this.$store.getters["themeID"];
+            const theme = new Theme(`t${id}`, "", []);
+
+            this.$store.commit( "addTheme", theme );
+            this.$store.commit( "incrementThemeID" );
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
