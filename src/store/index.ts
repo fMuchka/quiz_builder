feca06900b957dacc9eea6762ab59b6084dee354
fMuchka <f.muchka@gmail.com>
2021-12-30 16:46:10 +0100

    modified Store: getters, mutators, actions

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,8 @@ import { InjectionKey } from 'vue'
 import { createStore, Store } from "vuex";
 import { State } from '@/interfaces/State';
 import Quiz from '@/classes/Quiz';
+import Theme from '@/classes/Theme';
+import Question from '@/classes/Question';
 
 
 export const key: InjectionKey<Store<State>> = Symbol()
@@ -18,7 +20,6 @@ export default createStore<State>({
     tID: 0,
     qID: 0,
     mID: 0
-
   },
   mutations: {
     setQuiz(state, q) {
@@ -29,21 +30,78 @@ export default createStore<State>({
       state.themes.push(t);
 
       state.quiz.addTheme(t);
+      state.tID++;
     },
 
-    incrementThemeID(state) {
-      state.tID++;
+    deleteTheme(state, t) {
+      const tIndex = state.themes.findIndex(e => e === t);
+      state.themes.splice(tIndex, 1);
+
+      state.quiz.removeTheme(t);
+    },
+
+    addQuestion(state, { question, theme }) {
+      state.questions.push(question);
+
+      state.quiz.addQuestion(question);
+      state.qID++;
+
+      // due to private State, we cannot add questions to themes outside State
+      if (theme !== undefined) {
+        theme.addQuestion(question);
+      }
+    },
+
+    deleteQuestion(state, { question, theme }) {
+      const qIndex = state.questions.findIndex(e => e === question);
+      state.questions.splice(qIndex, 1);
+
+      state.quiz.removeQuestion(question);
+      theme.removeQuestion(question);
+    },
+
+    updateThemeLabel(state, { newLabel, theme }) {
+      theme.setLabel(newLabel)
+    },
+
+    updateQuestionText(state, { question, value }) {
+      question.setText(value);
+    },
+
+    updateQuestionAnswer(state, { question, value }) {
+      question.setAnswer(value);
+    },
+
+    updateQuestionMax(state, { question, value }) {
+      question.setMax(value);
+    },
+
+    updateQuestionIncrement(state, { question, value }) {
+      question.setIncrement(value);
     }
   },
   actions: {
-    createQuiz( { commit } ) {
-      commit("setQuiz", new Quiz);
+    createQuiz( { commit, getters } ) {
+      commit( "setQuiz", new Quiz );
+
+      // make sure Quiz has atleast one Theme.ts to start with
+      const themeID = getters["themeID"];
+      const theme = new Theme(themeID, "", []);
+
+      // and atleast one Question.ts
+      const questionID = getters["questionID"];
+      const question = new Question(questionID, undefined);
+
+      theme.addQuestion(question);
+      
+      commit("addTheme", theme);
+      commit("addQuestion", {question});
     }
   },
   modules: {},
   getters: {
     quiz: ( state ) => { return state.quiz },
-
-    themeID: ( state ) => { return state.tID }
+    themeID: (state) => { return `t${state.tID}` },
+    questionID: ( state ) => { return `q${state.qID}` },
   }
 });
